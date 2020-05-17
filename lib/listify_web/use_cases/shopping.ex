96cff60d67fa14b44e3546feea8b3f24f8df549c
Pedro Perrone <psperrone@hotmail.com>
2020-05-17 15:55:20 -0300

    Integrate shopping use cases with pub sub

--- lib/listify_web/use_cases/shopping.ex
+++ lib/listify_web/use_cases/shopping.ex
@@ -2,27 +2,50 @@ defmodule ListifyWeb.Shopping do
   alias Ecto.Changeset
   alias Listify.Shopping
   alias Listify.Shopping.Item
+  alias Phoenix.PubSub
+
+  @pub_sub_name Listify.PubSub
+  @items_topic "items"
 
   @spec list_items() :: [Item.t()]
   def list_items, do: Shopping.list_items()
 
   @spec create_item(map()) :: {:ok, Item.t()} | {:error, Changeset.t()}
-  def create_item(attrs), do: Shopping.create_item(attrs)
+  def create_item(attrs) do
+    with {:ok, item = %Item{}} <- Shopping.create_item(attrs) do
+      broadcast_to_items(item, :new_item)
+
+      {:ok, item}
+    end
+  end
 
   @spec update_item(binary(), map()) :: {:error, binary() | Changeset.t()} | {:ok, Item.t()}
   def update_item(id, attrs) do
-    with {:ok, item = %Item{}} <- Shopping.get_item(id) do
-      Shopping.update_item(item, attrs)
+    with {:ok, item = %Item{}} <- Shopping.get_item(id),
+         {:ok, item = %Item{}} <- Shopping.update_item(item, attrs) do
+      broadcast_to_items(item, :updated_item)
+
+      {:ok, item}
     end
   end
 
   @spec delete_item(binary) :: {:ok, Item.t()} | {:error, binary()}
   def delete_item(id) do
-    with {:ok, item = %Item{}} <- Shopping.get_item(id) do
-      Shopping.delete_item(item)
+    with {:ok, item = %Item{}} <- Shopping.get_item(id),
+         {:ok, item = %Item{}} <- Shopping.delete_item(item) do
+      broadcast_to_items(item, :deleted_item)
+
+      {:ok, item}
     end
   end
 
   @spec change_item(Item.t(), map()) :: Ecto.Changeset.t()
   def change_item(item = %Item{}, attrs \\ %{}), do: Shopping.change_item(item, attrs)
+
+  @spec subscribe_to_items :: :ok | {:error, {:already_registered, pid}}
+  def subscribe_to_items, do: PubSub.subscribe(@pub_sub_name, @items_topic)
+
+  @spec broadcast_to_items(any(), atom()) :: :ok
+  defp broadcast_to_items(message, event),
+    do: PubSub.broadcast!(@pub_sub_name, @items_topic, {event, message})
 end
--- test/listify_web/use_cases/shopping_test.exs
+++ test/listify_web/use_cases/shopping_test.exs
@@ -22,11 +22,26 @@ defmodule ListifyWeb.ShoppingTest do
       assert {:ok, item = %Item{}} = Shopping.create_item(attrs)
     end
 
+    test "broadcasts the new item to the items topic" do
+      Shopping.subscribe_to_items()
+      attrs = params_for(:item)
+      {:ok, item = %Item{}} = Shopping.create_item(attrs)
+
+      assert_received {:new_item, ^item}
+    end
+
     test "returns an invalid changeset when the parameters are invalid" do
       attrs = %{name: "", taken: false}
 
       assert {:error, changeset = %Changeset{}} = Shopping.create_item(attrs)
       refute changeset.valid?
+    end
+
+    test "does not broadcast in errors" do
+      attrs = %{name: "", taken: false}
+      Shopping.create_item(attrs)
+
+      refute_received _any_message
     end
   end
 
@@ -38,8 +53,22 @@ defmodule ListifyWeb.ShoppingTest do
       assert is_nil(Repo.get(Item, item.id))
     end
 
+    test "broadcasts the deletion of the item" do
+      Shopping.subscribe_to_items()
+      item = insert(:item)
+      {:ok, item = %Item{}} = Shopping.delete_item(item.id)
+
+      assert_received {:deleted_item, ^item}
+    end
+
     test "returns an error when the item does not exist" do
       assert {:error, "The item does not exist"} == Shopping.delete_item(UUID.generate())
+    end
+
+    test "does not broadcast in errors" do
+      Shopping.delete_item(UUID.generate())
+
+      refute_received _any_message
     end
   end
 
@@ -50,6 +79,14 @@ defmodule ListifyWeb.ShoppingTest do
       assert {:ok, updated_item = %Item{}} = Shopping.update_item(item.id, %{taken: true})
       assert updated_item.id == item.id
       assert updated_item.taken
+    end
+
+    test "broadcasts the updated item" do
+      Shopping.subscribe_to_items()
+      item = insert(:item)
+      {:ok, item = %Item{}} = Shopping.update_item(item.id, %{taken: true})
+
+      assert_received {:updated_item, ^item}
     end
 
     test "returns an error when the item does not exist" do
@@ -63,6 +100,13 @@ defmodule ListifyWeb.ShoppingTest do
       refute changeset.valid?
       assert errors_on(changeset) == %{name: ["can't be blank"]}
     end
+
+    test "does not broadcast in errors" do
+      item = insert(:item)
+      Shopping.update_item(item.id, %{name: ""})
+
+      refute_received _any_message
+    end
   end
 
   describe "change_item/2" do
@@ -73,6 +117,15 @@ defmodule ListifyWeb.ShoppingTest do
 
       assert changeset.changes == attrs
       assert changeset.data == item
+    end
+  end
+
+  describe "subscribe_to_items/0" do
+    test "subscribe the current process to the items topic" do
+      Shopping.subscribe_to_items()
+      Phoenix.PubSub.broadcast!(Listify.PubSub, "items", "ping")
+
+      assert_received "ping"
     end
   end
 end
