b5bd7c9809105c947a1750c5dc7d8c87517b9d0d
Pedro Perrone <psperrone@hotmail.com>
2020-05-17 12:48:10 -0300

    Fix items disappearing on apdate after delete (#7)

--- lib/listify_web/live/item_live/index.ex
+++ lib/listify_web/live/item_live/index.ex
@@ -15,23 +15,6 @@ defmodule ListifyWeb.ItemLive.Index do
   end
 
   @impl true
-  def handle_params(_params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action)}
-  end
-
-  defp apply_action(socket, :new) do
-    socket
-    |> assign(:page_title, "New Item")
-    |> assign(:item, %Item{})
-  end
-
-  defp apply_action(socket, :index) do
-    socket
-    |> assign(:page_title, "Listing Items")
-    |> assign(:item, nil)
-  end
-
-  @impl true
   def handle_event("delete", %{"id" => id}, socket) do
     with {:ok, item = %Item{}} <- Shopping.get_item(id),
          {:ok, item = %Item{}} <- Shopping.delete_item(item) do
@@ -49,7 +32,7 @@ defmodule ListifyWeb.ItemLive.Index do
          {:ok, item = %Item{}} <- Shopping.update_item(item, %{taken: !item.taken}) do
       {:noreply,
        socket
-       |> assign(:items, [item])
+       |> assign(items: [item], phx_update: "prepend")
        |> put_flash(:notice, "#{item.name} deleted")}
     else
       {:error, reason} -> {:noreply, put_flash(socket, :error, reason)}
