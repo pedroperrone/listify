245cd41b9a4ee8593c9f9345936521ae48e84bdd
Pedro Perrone <psperrone@hotmail.com>
2020-05-17 12:04:27 -0300

    Allow to mark item as taken (#6)
    * Allow to mark item as taken
    
    * Use temporary assigns

--- lib/listify_web/live/item_live/index.ex
+++ lib/listify_web/live/item_live/index.ex
@@ -6,7 +6,12 @@ defmodule ListifyWeb.ItemLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :items, fetch_items())}
+    socket =
+      socket
+      |> assign(items: fetch_items(), phx_update: "prepend")
+      |> assign(temporary_assigns: [items: [], phx_update: "prepend"])
+
+    {:ok, socket}
   end
 
   @impl true
@@ -32,7 +37,19 @@ defmodule ListifyWeb.ItemLive.Index do
          {:ok, item = %Item{}} <- Shopping.delete_item(item) do
       {:noreply,
        socket
-       |> assign(:items, fetch_items())
+       |> assign(items: fetch_items(), phx_update: "replace")
+       |> put_flash(:notice, "#{item.name} deleted")}
+    else
+      {:error, reason} -> {:noreply, put_flash(socket, :error, reason)}
+    end
+  end
+
+  def handle_event("toggle_taken", %{"id" => id}, socket) do
+    with {:ok, item = %Item{}} <- Shopping.get_item(id),
+         {:ok, item = %Item{}} <- Shopping.update_item(item, %{taken: !item.taken}) do
+      {:noreply,
+       socket
+       |> assign(:items, [item])
        |> put_flash(:notice, "#{item.name} deleted")}
     else
       {:error, reason} -> {:noreply, put_flash(socket, :error, reason)}
