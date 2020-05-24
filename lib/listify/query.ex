fc0b3d5d95e768e3cff4d570b1a09bc96ea30cb5
Pedro Perrone <psperrone@hotmail.com>
2020-05-24 18:43:20 -0300

    Inline all functions that fit in one line

--- lib/listify/query.ex
+++ lib/listify/query.ex
@@ -1,12 +1,9 @@
 defmodule Listify.Query do
   import Ecto.Query
 
-  def order(queriable, attribute, order \\ :asc) do
-    order_by(queriable, [{^order, ^attribute}])
-  end
+  def order(queriable, attribute, order \\ :asc), do: order_by(queriable, [{^order, ^attribute}])
 
-  def custom_filters(queriable, filters),
-    do: Enum.reduce(filters, queriable, &custom_query/2)
+  def custom_filters(queriable, filters), do: Enum.reduce(filters, queriable, &custom_query/2)
 
   defp custom_query({key, value}, queriable) when is_bitstring(key),
     do: custom_query({String.to_existing_atom(key), value}, queriable)
