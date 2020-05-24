9bdae7f161f52272529788dca504ce32eb55878d
Pedro Perrone <psperrone@hotmail.com>
2020-05-24 18:33:22 -0300

    Simplifies params filters logic

--- lib/listify_web/use_cases.ex
+++ lib/listify_web/use_cases.ex
@@ -13,11 +13,8 @@ defmodule ListifyWeb.UseCases do
   """
   @spec allowed_filters(map(), map()) :: map()
   def allowed_filters(filters, allowed_values) do
-    allowed_keys = Map.keys(allowed_values)
-
     filters
-    |> Enum.filter(fn {key, _value} -> key in allowed_keys end)
-    |> Enum.filter(fn {key, value} -> value in Map.get(allowed_values, key) end)
+    |> Enum.filter(fn {key, value} -> value in Map.get(allowed_values, key, []) end)
     |> Map.new()
   end
 
